86fb1013cdb38e212c587e78cefde1d50d1cc646
David Petr <d.petr.cz@gmail.com>
2023-09-11 08:35:51 -0700

    Add calculator buttons & style, create math ()s.

---
+++ script.js
@@ -0,0 +1,39 @@
+let a;
+let b;
+let operator;
+
+function operate(a, b, operator) {
+  if (operator == "+") {
+    return add(a,b);
+  } else if (operator == "-") {
+    return subtract(a, b);
+  } else if (operator == "*") {
+    return multiply(a, b);
+  } else if (operator == "/") {
+    return divide(a, b);
+  } else if (operator == "sqr") {
+    return square(a);
+  }
+}
+
+// MATH
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function square(a) {
+  return a * a;
+}
